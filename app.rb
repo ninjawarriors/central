402e7e31a113f5850a68a37f5de35921f57f43db
Steve Lum <steve.lum@gmail.com>
2011-11-03 21:10:28 -0700

    added new cluster queue for testing

--- app.rb
+++ app.rb
@@ -1,21 +1,28 @@
 require 'sinatra'
 
-def debug msg
-  puts "d-b #{msg}"
-end
+module Central
+  def debug msg
+    puts "d-b #{msg}"
+  end
 
 # further requires (models, helpers, core extensions etc. { but not 'middleware' because that should be grabbed up by Rack when appropriate })
-Dir.glob('./application/**/*.rb') do |file|
-  require file.gsub(/\.rb/, '') unless file.include?('middleware')
-end
+  Dir.glob('./application/**/*.rb') do |file|
+    require file.gsub(/\.rb/, '') unless file.include?('middleware')
+  end
 
-get '/' do
-  @title = 'CENTRAL'
+  get '/' do
+    @title = 'CENTRAL'
+    
+    erb :index
+  end
+
+  post '/servers' do
+    Resque.enqueue(ServerCreate, params[:name])
+    redirect to('/')
+  end
   
-  erb :index
-end
-
-post '/servers' do
-  Resque.enqueue(ServerCreate, params[:name])
-  redirect to('/')
+  post '/clusters' do
+    Resque.enqueue(ClusterCreate, params[:name])
+    redirect to('/')
+  end
 end
--- application/server/create.rb
+++ application/server/create.rb
@@ -1,11 +1,27 @@
-module ServerCreate
-  @queue = :server
-
-  def self.perform(name, options = {})
-    debug "Creating Server: #{name}"
+module Central
+  module ServerCreate
+    @queue = :server
+  
+    def self.perform(name, options = {})
+      sleep 1
+      debug "Creating Server: #{name}"
+    end
+  
+    def self.after_batch_actions(name, options = {})
+      # Do something interesting
+    end
   end
-
-  def self.after_batch_actions(name, options = {})
-    # Do something interesting
+  
+  module ClusterCreate
+    @queue = :cluster
+  
+    def self.perform(name, options = {})
+      sleep 1
+      debug "Creating Cluster: #{name}"
+    end
+  
+    def self.after_batch_actions(name, options = {})
+      # Do something interesting
+    end
   end
 end
