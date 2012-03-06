158f23bb59f84b397304ac07c8c79ca0d18c6af3
Edouard Swiac <edouard.swiac@gmail.com>
2012-03-05 19:41:53 -0800

    deploy class to perform Resque deployment jobs

---
+++ lib/central/deploy.rb
@@ -0,0 +1,26 @@
+class Central
+	class Deploy
+    @queue = "server_deployment"
+    def self.perform(object, object_id, command_id)
+      puts "Applying command #{command_id} to #{object}::#{object_id}"
+      
+      command = Command.new(command_id)
+
+      stdout = RedisTailer.new id, "stdout"
+      stderr = RedisTailer.new id, "stderr"
+
+      begin
+        status = spawn , 'stdout' => stdout, 'stderr' => stderr
+      rescue => e
+        error = e
+        puts e
+      end
+      puts command.props["command"]
+      puts status.to_i
+      puts stdout
+      puts stderr if stderr
+      ## log stuff here for this deployment
+
+    end
+	end
+end
