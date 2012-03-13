ec5006250c7dca1a2bc8433cb41fdd95e30f4d00
Edouard Swiac <edouard.swiac@gmail.com>
2012-03-13 15:10:08 -0700

    use Log in Deploy

--- lib/central/deploy.rb
+++ lib/central/deploy.rb
@@ -1,39 +1,30 @@
 class Central
   class Deploy
     @queue = "server_deployment"
+
     def self.perform(object, object_id, command_id)
       puts "Running command ##{command_id} to #{object}::#{object_id}"
 
-      command = Command.new(command_id)
-      puts "Running '#{command.props["command"]}'"
+      command = Command.new command_id
+      log = Log.new object_id
+      b_stdout = Log::Buffer.new object_id, "stdout"
+      b_stderr = Log::Buffer.new object_id, "stderr"
 
-      b_stdout = Command::ResultBuffer.new
-      b_stderr = Command::ResultBuffer.new
-      exit_status = -1
-      exception = ""
-      
-      started = Time.now.to_i
+      h = {}
+      h["exit_status"] = 1
+      h["started"] = Time.now.to_f
+      h["finished"] = nil
+      log.save h
+
       begin
-        status = spawn command.props["command"], 'stdout' => b_stdout, 'stderr' => b_stderr
+        h["command"] = command.props["command"]
+        h["exit_status"] = spawn command.props["command"],'stdout' => b_stdout,'stderr' => b_stderr
       rescue => e
-        exception = e.strip!
+        h["error"] = e.strip!
       end
 
-      complete = Time.now.to_i
-      
-      result = Command::Result.new
-      result.object_key = object
-      result.object_id = object_id
-      result.exit_status= exit_status.to_i
-      result.timestamp_start = started
-      result.timestamp_complete = complete
-      result.stdout = b_stdout.flush if not b_stdout.empty? 
-      result.stderr = b_stderr.flush if not b_stderr.empty?
-      result.exception = exception if not exception.empty?
-
-      #Central.redis.rpush "commands::#{command_id}::results", result.to_s
-      Central.redis.rpush "#{object}::#{object_id}::command_results", result.to_s
-
+      h["finished"] = Time.now.to_f
+      log.save h
     end
   end
 end
