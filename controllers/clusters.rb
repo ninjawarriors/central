341de0a1cf771b710f3fcaa89734f90d69d3d714
Steve Lum <steve.lum@gmail.com>
2012-10-24 23:04:49 -0700

    testing deploy versions. also removed empty lines

--- controllers/clusters.rb
+++ controllers/clusters.rb
@@ -1,10 +1,8 @@
 class Central
-
   get '/clusters' do
     @crumbs = []
     @crumbs << Central.crumb("Dashboard", "/")
     @active = Central.crumb("Clusters", "/clusters")
-
     @clusters = Cluster.list_all
     haml "clusters/list"
   end
@@ -14,7 +12,6 @@ class Central
     @crumbs << Central.crumb("Dashboard", "/")
     @crumbs << Central.crumb("Clusters", "/clusters")
     @active = Central.crumb("Create")
-
     @environments = Environment.list_all
     haml "clusters/create"
   end
@@ -22,7 +19,6 @@ class Central
   get '/clusters/:cluster' do |c_id|
     pass if c_id == "create"
     @cluster = Cluster.new(c_id)
-    
     @crumbs = []
     @crumbs << Central.crumb("Dashboard", "/")
     @crumbs << Central.crumb("Clusters", "/clusters")
@@ -32,14 +28,15 @@ class Central
 
   post '/clusters' do
     id = counter
-
     c = Cluster.new(id)
     c.save(params)
-
     e = Environment.new(params["environment"])
     e.add_cluster(params["environment_id"],id)
-
     redirect to('/clusters')
   end
 
+  post '/deploys' do
+    id = counter
+    Resque.enqueue(CommandRun, Central.counter, params['version'])
+  end
 end
