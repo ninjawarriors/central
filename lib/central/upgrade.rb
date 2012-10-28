493fdbc061efb09e76e94a9d3e01990cb88a77a8
Steve Lum <steve.lum@gmail.com>
2012-10-28 15:42:22 -0700

    fixed the upgrade module. testing ssh command to node ip's stored in redis. saving command and output to log in redis as well

--- lib/central/upgrade.rb
+++ lib/central/upgrade.rb
@@ -2,21 +2,30 @@ class Central
 	module Upgrade
 		@queue = "upgrade"
 
-		def self.perform(object, object_id, command_id)
-			Central.debug "Upgrading Cluster"
+		def self.perform(ip, version)
+			Central.debug "Upgrading Cluster Node #{ip} to version #{version}"
+			h = {}
+			h['started'] = Time.now.to_f
 			log = Log.new object_id
 			b_stdout = Log::Buffer.new object_id, "stdout"
       b_stderr = Log::Buffer.new object_id, "stderr"
-			h['started'] = Time.now.to_f
-			h['finished'] = nil
-			log.save h
+			command = "ssh -p 22223 #{ip} 'cat /etc/issue'"
 
 			begin
-				h['command'] = command
-				h["exit_status"] = spawn command,'stdout' => b_stdout,'stderr' => b_stderr
+				status = spawn command, 'stdout' => b_stdout, 'stderr' => b_stderr
 			rescue => e
-				h["error"] = e.strip!
+				h["error"] = e
 			end
+			h['finished'] = Time.now.to_f
+			log.save h
+
+
+			if DEBUG
+        puts command
+        puts status.to_i
+        puts b_stdout
+        puts b_stderr if b_stderr
+      end
 		end
 
 	end
