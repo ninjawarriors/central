3aa0c5ef809c5429cd7d147e20fac099b7433a8c
Steve Lum <steve.lum@gmail.com>
2012-11-09 01:11:53 -0800

    updating Deploy class with new methods to install packages on a node bootstrap

--- lib/central/deploy.rb
+++ lib/central/deploy.rb
@@ -2,7 +2,75 @@ class Central
   class Deploy
     @queue = "server_deployment"
 
-    def self.perform(object, object_id, command_id)
+    def self.perform(object, object_id, ip)
+      Central.debug "Bootstrapping Node #{ip}"
+
+      install_epel(ip)
+      install_curl(ip)
+    end
+
+    def self.install_curl(ip)
+      Central.debug "Installing curl on Node #{ip}"
+
+      command = "ssh -p 22 root@#{ip} 'yum install -y curl'"
+      log = Log.new object_id
+      b_stdout = Log::Buffer.new object_id, "stdout"
+      b_stderr = Log::Buffer.new object_id, "stderr"
+
+      h = {}
+      h["exit_status"] = 1
+      h["started"] = Time.now.to_f
+      h["finished"] = nil
+      log.save h
+
+      begin
+        status = spawn command, 'stdout' => b_stdout, 'stderr' => b_stderr
+      rescue => e
+        h["error"] = e
+      end
+      h["finished"] = Time.now.to_f
+      log.save h
+
+      if DEBUG
+        puts command
+        puts status.to_i
+        puts b_stdout
+        puts b_stderr if b_stderr
+      end
+    end
+
+    def self.install_epel(ip)
+      Central.debug "Installing Epel on Node #{ip}"
+
+      command = "ssh -p 22 root@#{ip} 'rpm -ivh http://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-7.noarch.rpm'"
+      log = Log.new object_id
+      b_stdout = Log::Buffer.new object_id, "stdout"
+      b_stderr = Log::Buffer.new object_id, "stderr"
+
+      h = {}
+      h["exit_status"] = 1
+      h["started"] = Time.now.to_f
+      h["finished"] = nil
+      log.save h
+
+      begin
+        status = spawn command, 'stdout' => b_stdout, 'stderr' => b_stderr
+      rescue => e
+        h["error"] = e
+      end
+      h["finished"] = Time.now.to_f
+      log.save h
+
+      if DEBUG
+        puts command
+        puts status.to_i
+        puts b_stdout
+        puts b_stderr if b_stderr
+      end
+    end
+
+
+    def self.perform_old(object, object_id, command_id)
       puts "Running command ##{command_id} to #{object}::#{object_id}"
 
       command = Command.new command_id
