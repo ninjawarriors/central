5bbd872fb2d5a4e151ae661646678cd89d7d961b
Steve Lum <steve.lum@gmail.com>
2012-11-09 08:18:34 -0800

    trying to be DRY

--- lib/central/deploy.rb
+++ lib/central/deploy.rb
@@ -9,10 +9,9 @@ class Central
       install_curl(ip)
     end
 
-    def self.install_curl(ip)
-      Central.debug "Installing curl on Node #{ip}"
-
-      command = "ssh -p 22 root@#{ip} 'yum install -y curl'"
+    def self.queue(debug, cmd, ip)
+      Central.debug debug
+      command = "ssh -p 22 root@#{ip} '#{cmd}'"
       log = Log.new object_id
       b_stdout = Log::Buffer.new object_id, "stdout"
       b_stderr = Log::Buffer.new object_id, "stderr"
@@ -39,34 +38,18 @@ class Central
       end
     end
 
+    def self.install_curl(ip)
+      debug = "Installing curl on Node #{ip}"
+      cmd = "yum install -y curl"
+      ip = ip
+      queue(debug, cmd, ip)
+    end
+
     def self.install_epel(ip)
-      Central.debug "Installing Epel on Node #{ip}"
-
-      command = "ssh -p 22 root@#{ip} 'rpm -ivh http://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-7.noarch.rpm'"
-      log = Log.new object_id
-      b_stdout = Log::Buffer.new object_id, "stdout"
-      b_stderr = Log::Buffer.new object_id, "stderr"
-
-      h = {}
-      h["exit_status"] = 1
-      h["started"] = Time.now.to_f
-      h["finished"] = nil
-      log.save h
-
-      begin
-        status = spawn command, 'stdout' => b_stdout, 'stderr' => b_stderr
-      rescue => e
-        h["error"] = e
-      end
-      h["finished"] = Time.now.to_f
-      log.save h
-
-      if DEBUG
-        puts command
-        puts status.to_i
-        puts b_stdout
-        puts b_stderr if b_stderr
-      end
+      debug = "Installing Epel on Node #{ip}"
+      cmd = "rpm -ivh http://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-7.noarch.rpm"
+      ip = ip
+      queue(debug, cmd, ip)
     end
 
 
