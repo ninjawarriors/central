90f35d86837efee29765fedc2fca561e98aa874e
Steve Lum <steve.lum@gmail.com>
2012-11-12 21:14:49 -0800

    added new method in Deploy class to copy node json file to remote server

--- lib/central/deploy.rb
+++ lib/central/deploy.rb
@@ -2,16 +2,46 @@ class Central
   class Deploy
     @queue = "server_deployment"
 
-    def self.perform(object, object_id, ip)
+    def self.perform(object, object_id, ip, node_id, node_name)
       Central.debug "Bootstrapping Node #{ip}"
 
+      copy_node_json(ip, node_id, node_name)
       install_epel(ip)
       install_curl(ip)
       curl_repo(ip)
       setup_deps(ip)
       env_reset(ip)
-      copy(ip)
-      chef_solo(ip)
+      copy_databag(ip)
+      chef_solo(ip, node_id, node_name)
+    end
+
+    def self.copy_node_json(ip, node_id, node_name)
+      debug = "Copying node json file to Node #{ip}"
+      command = "scp /tmp/#{node_id}-#{node_name}.json root@#{ip}:/root"
+      log = Log.new object_id
+      b_stdout = Log::Buffer.new object_id, "stdout"
+      b_stderr = Log::Buffer.new object_id, "stderr"
+
+      h = {}
+      h["exit_status"] = 1
+      h["started"] = Time.now.to_f
+      h["finished"] = nil
+      log.save h
+
+      begin
+        status = spawn command, 'stdout' => b_stdout, 'stderr' => b_stderr
+      rescue => e
+        h["error"] = e
+      end
+      h["finished"] = Time.now.to_f
+      log.save h
+
+      if DEBUG
+        puts command
+        puts status.to_i
+        puts b_stdout
+        puts b_stderr if b_stderr
+      end
     end
 
     def self.install_curl(ip)
@@ -49,16 +79,16 @@ class Central
       queue(debug, cmd, ip)
     end
 
-    def self.copy(ip)
+    def self.copy_databag(ip)
       debug = "Copying files to the correct locations on Node #{ip}"
       cmd = "cp foo1.json /root/data_bags/accounts"
       ip = ip
       queue(debug, cmd, ip)
     end
 
-    def self.chef_solo(ip)
+    def self.chef_solo(ip, node_id, node_name)
       debug = "Running Chef Solo on Node #{ip}"
-      cmd = "chef-solo -j node.json -c solo.rb -l debug"
+      cmd = "chef-solo -j #{node_id}-#{node_name}.json -c solo.rb -l debug"
       ip = ip
       queue(debug, cmd, ip)
     end
